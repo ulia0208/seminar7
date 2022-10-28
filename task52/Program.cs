c352cbf2968f146376a1d5df1dc3dd1fc8b33238
Uliana Tokareva <ulia0208@yandex.ru>
2022-10-28 03:40:51 +0300

    task 52

--- task52/Program.cs
+++ task52/Program.cs
@@ -2,6 +2,51 @@
 // Найдите среднее арифметическое элементов в каждом столбце.
 
 
-Console.Write("Введите количество строк");
 
-Console.Write("Введите количество столбцов");
+
+void InputMatrix(int[,] matrix)
+{
+    for(int i=0;i<matrix.GetLength(0);i++)
+    {
+        for (int j=0;j<matrix.GetLength(1);j++)
+        {
+            matrix[i,j]=new Random().Next(-10,10);
+            Console.Write($"{matrix[i,j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов ");
+int m =Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n,m];
+InputMatrix(matrix);
+
+Console.WriteLine("Среднее арифметическое ");
+
+void SumColumn (int[,] matrix)
+{
+    float[] sum = new float[m];
+    float [] middle=new float[m];
+    for (int i=0; i<matrix.GetLength(1);i++)
+    {
+        for(int j=0; j<matrix.GetLength(0); j++)
+        {
+            sum[i]+= matrix[j,i];
+        }
+       // Console.Write($"{sum[i]} \t");
+        middle[i]=sum[i]/n;
+        Console.Write($"{Math.Round(middle[i]*1000)/1000} \t");
+        
+    }
+    Console.WriteLine();
+    
+}
+
+SumColumn(matrix);
+
+
